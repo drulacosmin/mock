951ab004b273f1a0207dee8cbce497ac9e1be99c
drulacosmin <drulacosmin@gmail.com>
2021-10-07 04:23:28 +0000

    add axios call

---
+++ client/src/App.js
@@ -0,0 +1,36 @@
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import Axios from "axios";
+
+function App() {
+  Axios.get('https://api.github.com/users/drulacosmin')
+  .then((response) => {
+    console.log(response.data);
+    console.log(response.status);
+    console.log(response.statusText);
+    console.log(response.headers);
+    console.log(response.config);
+  });
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
